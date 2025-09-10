f3b3e0a9541b9ded0020fff34462d8497b359398
Daniil Kachur <daniilkachur1704@gmail.com>
2025-09-10 20:57:57 +0300

    feat(domain): add product entity

---
+++ eCommerce.ProductsService.Domain/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿namespace eCommerce.ProductsService.Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } 
+    public string Category { get; private set; }
+    public double UnitPrice { get; private set; }
+    public int QuantityInStock { get; private set; }
+
+    public static Product New(string name, string category, double unitPrice, int quantityInStock)
+        => new()
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Category = category,
+            UnitPrice = unitPrice,
+            QuantityInStock = quantityInStock
+        };
+
+    public static Product Restore(Guid id, string name, string category, double unitPrice, int quantityInStock)
+        => new()
+        {
+            Id = id,
+            Name = name,
+            Category = category,
+            UnitPrice = unitPrice,
+            QuantityInStock = quantityInStock
+        };
+
+    private Product() { }
+}
