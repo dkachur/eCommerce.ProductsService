a1be371137df2af725534566da7fde28b8069d98
Daniil Kachur <daniilkachur1704@gmail.com>
2025-09-15 19:47:51 +0300

    docs(domain): add xml documentation

--- eCommerce.ProductsService.Domain/Entities/Product.cs
+++ eCommerce.ProductsService.Domain/Entities/Product.cs
@@ -1,13 +1,44 @@
 ﻿namespace eCommerce.ProductsService.Domain.Entities;
 
+/// <summary>
+/// The product entity class.
+/// </summary>
 public class Product
 {
+    /// <summary>
+    /// The unique identifier of the product.
+    /// </summary>
     public Guid Id { get; private set; }
-    public string Name { get; private set; } 
+
+    /// <summary>
+    /// The name of the product.
+    /// </summary>
+    public string Name { get; private set; }
+
+    /// <summary>
+    /// The category to which the product belongs.
+    /// </summary>
     public string Category { get; private set; }
+
+    /// <summary>
+    /// The price of a single product unit.
+    /// </summary>
     public double UnitPrice { get; private set; }
+
+    /// <summary>
+    /// The number of items available in stock.
+    /// </summary>
     public int QuantityInStock { get; private set; }
 
+
+    /// <summary>
+    /// Creates new product entity with newly generated ID.
+    /// </summary>
+    /// <param name="name">The name of the product.</param>
+    /// <param name="category">The category to which the product belongs.</param>
+    /// <param name="unitPrice">The price of a single product unit.</param>
+    /// <param name="quantityInStock">The number of items available in stock.</param>
+    /// <returns></returns>
     public static Product New(string name, string category, double unitPrice, int quantityInStock)
         => new()
         {
@@ -18,6 +49,15 @@ public class Product
             QuantityInStock = quantityInStock
         };
 
+    /// <summary>
+    /// Reconstructs a user with the specified details.
+    /// </summary>
+    /// <param name="id">The unique identifier of the product.</param>
+    /// <param name="name">The name of the product.</param>
+    /// <param name="category">The category to which the product belongs.</param>
+    /// <param name="unitPrice">The price of a single product unit.</param>
+    /// <param name="quantityInStock">The number of items available in stock.</param>
+    /// <returns></returns>
     public static Product Restore(Guid id, string name, string category, double unitPrice, int quantityInStock)
         => new()
         {
@@ -28,5 +68,9 @@ public class Product
             QuantityInStock = quantityInStock
         };
 
+    /// <summary>
+    /// Private constructor to prevent direct instantiation.
+    /// Use factory methods to create instances of <see cref="Product"/>.
+    /// </summary>
     private Product() { }
 }
