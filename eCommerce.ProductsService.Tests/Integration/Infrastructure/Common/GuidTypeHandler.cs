1570583e4fca21ae79b93f5e4f6db28d5710a583
Daniil Kachur <daniilkachur1704@gmail.com>
2025-11-14 16:07:16 +0200

    test(products-repository): add some integration tests for ProductsRepository (WIP)

---
+++ eCommerce.ProductsService.Tests/Integration/Infrastructure/Common/GuidTypeHandler.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using System.Data;
+
+namespace eCommerce.ProductsService.Tests.Integration.Infrastructure.Common;
+
+public class GuidTypeHandler : SqlMapper.TypeHandler<Guid>
+{
+    public override Guid Parse(object value)
+    {
+        var str = value.ToString();
+
+        if (str is not null)
+            return Guid.Parse(str);
+        else
+            return Guid.Empty;
+    }
+
+    public override void SetValue(IDbDataParameter parameter, Guid value)
+        => parameter.Value = value.ToString();
+}
