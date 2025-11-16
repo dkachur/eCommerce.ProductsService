3885b8b9ece99ec9b6c9219ce70b18807eeb5053
Daniil Kachur <110394635+dkachur@users.noreply.github.com>
2025-11-16 14:44:00 +0200

    test(message-publishers): add unit tests for ProductUpdatedPublisher, ProductDeletedPublisher and RabbitMqPublisher
    Adds unit tests for all message publisher implementations.

---
+++ eCommerce.ProductsService.Tests/Infrastructure/Messaging/Publishers/ProductDeletedPublisherTest.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+using eCommerce.ProductsService.Application.Messaging;
+using eCommerce.ProductsService.Infrastructure.Messaging.Interfaces;
+using eCommerce.ProductsService.Infrastructure.Messaging.Options;
+using eCommerce.ProductsService.Infrastructure.Messaging.Publishers;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace eCommerce.ProductsService.Tests.Infrastructure.Messaging.Publishers;
+
+public class ProductDeletedPublisherTest
+{
+    private readonly IMessagePublisher<ProductDeletedMessage> _publisher;
+
+    private readonly Mock<IRabbitMqPublisher> _mqPublisherMock;
+    private readonly Faker _faker;
+
+    private const string RoutingKey = "product.deleted";
+
+    public ProductDeletedPublisherTest()
+    {
+        _faker = new Faker();
+        var options = Options.Create(new RabbitMqOptions() { ProductDeletedRoutingKey = RoutingKey });
+        _mqPublisherMock = new Mock<IRabbitMqPublisher>();
+
+        _publisher = new ProductDeletedPublisher(_mqPublisherMock.Object, options);
+    }
+
+    [Fact]
+    public async Task PublishAsync_ShouldPublishMessage_WhenCalled()
+    {
+        // Arrange
+        var message = new ProductDeletedMessage(_faker.Random.Guid());
+
+
+        // Act
+        await _publisher.PublishAsync(message);
+
+
+        // Assert
+        _mqPublisherMock.Verify(p => p.PublishAsync(message, RoutingKey, It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
---
+++ eCommerce.ProductsService.Tests/Infrastructure/Messaging/Publishers/ProductUpdatedPublisherTests.cs
@@ -0,0 +1,49 @@
+﻿using Bogus;
+using eCommerce.ProductsService.Application.Enums;
+using eCommerce.ProductsService.Application.Messaging;
+using eCommerce.ProductsService.Infrastructure.Messaging.Interfaces;
+using eCommerce.ProductsService.Infrastructure.Messaging.Options;
+using eCommerce.ProductsService.Infrastructure.Messaging.Publishers;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace eCommerce.ProductsService.Tests.Infrastructure.Messaging.Publishers;
+
+public class ProductUpdatedPublisherTests
+{
+    private readonly IMessagePublisher<ProductUpdatedMessage> _publisher;
+
+    private readonly Mock<IRabbitMqPublisher> _mqPublisherMock;
+    private readonly Faker _faker;
+
+    private const string RoutingKey = "product.updated";
+
+    public ProductUpdatedPublisherTests()
+    {
+        _faker = new Faker();
+        var options = Options.Create(new RabbitMqOptions() { ProductUpdatedRoutingKey = "product.updated" });
+        _mqPublisherMock = new Mock<IRabbitMqPublisher>();
+
+        _publisher = new ProductUpdatedPublisher(_mqPublisherMock.Object, options);
+    }
+
+    [Fact]
+    public async Task PublishAsync_ShouldPublishMessage_WhenCalled()
+    {
+        // Arrange
+        var message = new ProductUpdatedMessage(
+            _faker.Random.Guid(),
+            _faker.Commerce.ProductName(),
+            _faker.Random.Enum<CategoryOptions>().ToString(),
+            _faker.Random.Double(1, 100),
+            _faker.Random.Int(0, 100));
+
+
+        // Act
+        await _publisher.PublishAsync(message);
+
+
+        // Assert
+        _mqPublisherMock.Verify(p => p.PublishAsync(message, RoutingKey, It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
