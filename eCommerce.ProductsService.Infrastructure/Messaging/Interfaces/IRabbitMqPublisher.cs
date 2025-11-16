323f752ec08682b3d4143d4dd6b9807386c14e3c
Daniil Kachur <daniilkachur1704@gmail.com>
2025-11-16 10:38:36 +0200

    refactor(messaging): add IRabbitMqPublisher interface for RabbitMqPublisher

---
+++ eCommerce.ProductsService.Infrastructure/Messaging/Interfaces/IRabbitMqPublisher.cs
@@ -0,0 +1,6 @@
+﻿namespace eCommerce.ProductsService.Infrastructure.Messaging.Interfaces;
+
+public interface IRabbitMqPublisher : IAsyncDisposable
+{
+    Task PublishAsync<T>(T message, string routingKey, CancellationToken ct = default);
+}
