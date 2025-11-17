e7c7c15f5ee0ab823856beafff7d1a3d0827562c
Daniil Kachur <daniilkachur1704@gmail.com>
2025-11-17 13:23:03 +0200

    test(products-service): add ProductFactory helper and update service tests

---
+++ eCommerce.ProductsService.Tests/Helpers/ProductFactory.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using eCommerce.ProductsService.Application.Enums;
+using eCommerce.ProductsService.Domain.Entities;
+
+namespace eCommerce.ProductsService.Tests.Helpers;
+
+public static class ProductFactory
+{
+    private static readonly Faker _faker = new();
+
+    public static Product CreateRandom()
+    {
+        return Product.Restore(
+            _faker.Random.Guid(),
+            _faker.Commerce.ProductName(),
+            _faker.Random.Enum<CategoryOptions>().ToString(),
+            _faker.Random.Double(1, 100),
+            _faker.Random.Int(0, 100));
+    }
+}
