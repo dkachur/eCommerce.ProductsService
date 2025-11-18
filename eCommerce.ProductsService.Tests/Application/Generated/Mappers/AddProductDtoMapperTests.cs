4873194860010d7cf81a114aa0940bad2db65c35
Daniil Kachur <110394635+dkachur@users.noreply.github.com>
2025-11-18 14:11:58 +0200

    test(mappers): add unit tests for all mappers in API and Application layers

---
+++ eCommerce.ProductsService.Tests/Application/Generated/Mappers/AddProductDtoMapperTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture;
+using eCommerce.ProductsService.Application.DTOs;
+using eCommerce.ProductsService.Domain.Entities;
+using FluentAssertions;
+
+namespace eCommerce.ProductsService.Tests.Application.Generated.Mappers;
+
+public class AddProductDtoMapperTests
+{
+    private readonly Fixture _fixture;
+
+    public AddProductDtoMapperTests()
+    {
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public void AdaptToProduct_ShouldMapCorrectly_WhenCalled()
+    {
+        // Arrange
+        var addProductDto = _fixture.Create<AddProductDto>();
+        var expected = Product.New(
+            addProductDto.Name,
+            addProductDto.Category.ToString(),
+            addProductDto.UnitPrice,
+            addProductDto.QuantityInStock);
+
+
+        // Act
+        var product = addProductDto.AdaptToProduct();
+
+
+        // Assert
+        product.Should().BeEquivalentTo(expected, opt => opt.Excluding(p => p.Id));
+    }
+}
---
+++ eCommerce.ProductsService.Tests/Application/Generated/Mappers/ProductIEnumerableMapperTests.cs
@@ -0,0 +1,33 @@
+﻿using eCommerce.ProductsService.Application.DTOs;
+using eCommerce.ProductsService.Application.Enums;
+using eCommerce.ProductsService.Tests.Helpers;
+using FluentAssertions;
+
+namespace eCommerce.ProductsService.Tests.Application.Generated.Mappers;
+
+public class ProductIEnumerableMapperTests
+{
+    [Fact]
+    public void AdaptToProductDtoList_ShouldMapCorrectly_WhenCalled()
+    {
+        // Arrange
+        var products = Enumerable.Range(0, 10)
+            .Select(_ => ProductFactory.CreateRandom())
+            .ToList();
+
+        var expected = products
+            .Select(p => new ProductDto(
+                p.Id,
+                p.Name,
+                Enum.Parse<CategoryOptions>(p.Category),
+                p.UnitPrice,
+                p.QuantityInStock))
+            .ToList();
+
+        // Act
+        var productDtos = products.AdaptToProductDtoList();
+
+        // Assert
+        productDtos.Should().BeEquivalentTo(expected);
+    }  
+}
