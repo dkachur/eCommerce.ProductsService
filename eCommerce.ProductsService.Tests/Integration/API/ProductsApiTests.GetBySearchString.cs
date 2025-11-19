ec7e7da8cb430b03a32586215ce88a1a7b2e2315
Daniil Kachur <daniilkachur1704@gmail.com>
2025-11-19 11:00:46 +0200

    test(api): add integration tests for all Products API endpoints

---
+++ eCommerce.ProductsService.Tests/Integration/API/ProductsApiTests.GetBySearchString.cs
@@ -0,0 +1,44 @@
+﻿using eCommerce.ProductsService.API.DTOs;
+using eCommerce.ProductsService.Application.Enums;
+using eCommerce.ProductsService.Tests.Integration.Common;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace eCommerce.ProductsService.Tests.Integration.API;
+
+public partial class ProductsApiTests
+{
+    [Fact(DisplayName = "GET /api/products/search/{searchString} should return products which Name or Category contain search string")]
+    public async Task GetProductBySearchString_ShouldReturnCorrectProducts_WhenRequest()
+    {
+        // Arrange
+        var searchString = "ele";
+
+        var additionalProducts = new List<AddProductRequest>
+        {
+            new("Electric watches", CategoryOptions.Accessories.ToString(), 129, 4),
+            new("Elementary school book", CategoryOptions.HomeAppliances.ToString(), 2, 56),
+            new("Chemistry elements table", CategoryOptions.HomeAppliances.ToString(), 3.49, 20),
+            new("Ceramic elements mug", CategoryOptions.Furniture.ToString(), 12, 8),
+        };
+
+        var productResponses = await SeedProductsAsync(10, additionalProducts);
+
+        var expected = productResponses
+            .Where(p => 
+                p.Name.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
+                || p.Category.ToString().Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+            .ToList();
+
+
+        // Act
+        var response = await _client.GetAsync($"/api/products/search/{searchString}");
+        var productsFromApi = await response.Content.ReadFromJsonAsync<List<ProductResponse>>(JsonConfig.Options);
+
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        productsFromApi.Should().BeEquivalentTo(expected, opt => opt.WithoutStrictOrdering());
+    }
+}
---
+++ eCommerce.ProductsService.Tests/Integration/API/ProductsApiTests.GetProducts.cs
@@ -0,0 +1,27 @@
+﻿using eCommerce.ProductsService.API.DTOs;
+using eCommerce.ProductsService.Tests.Integration.Common;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace eCommerce.ProductsService.Tests.Integration.API;
+
+public partial class ProductsApiTests
+{
+    [Fact(DisplayName = "GET /api/products should return all products with 200 OK status")]
+    public async Task GetProducts_ShouldReturnAllProducts_WhenRequest()
+    {
+        // Arrange
+        var expected = await SeedProductsAsync(10);
+
+
+        // Act
+        var response = await _client.GetAsync("/api/products");
+        var productsFromApi = await response.Content.ReadFromJsonAsync<List<ProductResponse>>(JsonConfig.Options);
+
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        productsFromApi.Should().BeEquivalentTo(expected, opt => opt.WithoutStrictOrdering());
+    }
+}
