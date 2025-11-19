424c659d09c39333b16f5a1f9d8099230dc22ab7
Daniil Kachur <110394635+dkachur@users.noreply.github.com>
2025-11-19 11:06:37 +0200

    test(api): add integration tests covering all Products API endpoints
    * test(api): add ProductsApiFactory and update IntegrationTestsFixture for HTTP integration tests
    
    * test(api): add integration test setup in ProductsApiTests.Setup
    
    * test(api): add integration tests for all Products API endpoints

---
+++ eCommerce.ProductsService.Tests/Integration/API/ProductsApiFactory.cs
@@ -0,0 +1,45 @@
+﻿using eCommerce.ProductsService.Application.Messaging;
+using eCommerce.ProductsService.Infrastructure.Messaging.Interfaces;
+using eCommerce.ProductsService.Tests.Integration.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using System.Data;
+
+namespace eCommerce.ProductsService.Tests.Integration.API;
+
+public class ProductsApiFactory : WebApplicationFactory<Program>
+{
+    private TestDatabase _db;
+    private IMessagePublisher<ProductUpdatedMessage>? _updatedPublisher;
+    private IMessagePublisher<ProductDeletedMessage>? _deletedPublisher;
+
+    public ProductsApiFactory(TestDatabase db)
+    {
+        _db = db;
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        _updatedPublisher = new Mock<IMessagePublisher<ProductUpdatedMessage>>().Object;
+        _deletedPublisher = new Mock<IMessagePublisher<ProductDeletedMessage>>().Object;
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<IDbConnection>();
+            services.AddTransient<IDbConnection>(_ => _db.Connection);
+
+            services.RemoveAll<IRabbitMqConnectionManager>();
+            services.RemoveAll<IRabbitMqPublisher>();
+            services.RemoveAll<IMessagePublisher<ProductUpdatedMessage>>();
+            services.RemoveAll<IMessagePublisher<ProductDeletedMessage>>();
+
+            services.AddSingleton<IMessagePublisher<ProductUpdatedMessage>>(_ => _updatedPublisher);
+            services.AddSingleton<IMessagePublisher<ProductDeletedMessage>>(_ => _deletedPublisher);
+        });
+    }
+}
---
+++ eCommerce.ProductsService.Tests/Integration/API/ProductsApiTests.PostProduct.cs
@@ -0,0 +1,71 @@
+﻿using eCommerce.ProductsService.API.DTOs;
+using eCommerce.ProductsService.Application.Enums;
+using eCommerce.ProductsService.Tests.Helpers;
+using eCommerce.ProductsService.Tests.Integration.Common;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace eCommerce.ProductsService.Tests.Integration.API;
+
+public partial class ProductsApiTests
+{
+    [Fact(DisplayName = "POST /api/products should add product and return 201 Created")]
+    public async Task PostProduct_ShouldReturnCreatedProduct_WhenRequest()
+    {
+        // Arrange
+        var request = AddProductRequestFactory.CreateRandom();
+        var expected = new ProductResponse(
+            Guid.Empty,
+            request.Name,
+            Enum.Parse<CategoryOptions>(request.Category),
+            request.UnitPrice,
+            request.QuantityInStock);
+
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/products", request, JsonConfig.Options);
+        var created = await response.Content.ReadFromJsonAsync<ProductResponse>(JsonConfig.Options);
+
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        created.Should().BeEquivalentTo(expected, opt => opt.Excluding(p => p.Id));
+        created.Id.Should().NotBeEmpty();
+    }
+
+    [Fact(DisplayName = "POST /api/products should return ProblemDetails with validation errors and 400 Bad Request when request is invalid")]
+    public async Task PostProduct_ShouldReturnBadRequest_WhenValidationErrors()
+    {
+        // Arrange
+        var request = AddProductRequestFactory.CreateRandom() with 
+        { 
+            Name = new string('A', 51),
+            Category = "test-category",
+            UnitPrice = -100,
+            QuantityInStock = -1
+        };
+
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/products", request, JsonConfig.Options);
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>(JsonConfig.Options);
+
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        problemDetails.Should().NotBeNull();
+        problemDetails.Extensions.Should().ContainKey("errors");
+
+        var errorsElement = problemDetails.Extensions["errors"] as JsonElement?;
+        var errors = errorsElement?.Deserialize<Dictionary<string, string[]>>(JsonConfig.Options);
+
+        errors.Should().ContainKeys(
+            "Name",
+            "Category",
+            "UnitPrice",
+            "QuantityInStock");
+    }
+}
